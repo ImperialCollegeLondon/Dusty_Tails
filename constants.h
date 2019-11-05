b87ca7b48a00f070c8a33e64e07a82f6e90416d2
Beatriz Campos Estrada <52075738+beatrizestrada@users.noreply.github.com>
2019-11-05 12:45:05 +0000

    constants header file

---
+++ constants.h
@@ -0,0 +1,12 @@
+//Header file with constants
+
+
+#define PI 3.14159
+
+#define G 6.67408e-11
+#define Msun 1.9885e+30
+#define Rsun 6.9551e+8
+#define AU_to_m 1.496e+11
+#define m_to_AU 6.68459e-12
+
+
