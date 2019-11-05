3b928d426e9229399663e3a09078894ed5ea218e
Beatriz Campos Estrada <52075738+beatrizestrada@users.noreply.github.com>
2019-11-05 12:45:55 +0000

    Butcher table for Dormand-Prince method

---
+++ butcher.h
@@ -0,0 +1,43 @@
+//Header file with values of Butcher table for Dormand-Prince method defined
+
+#define a21 1.0/5.0
+
+#define a31 3.0/40.0
+#define a32 9.0/40.0
+
+#define a41 44.0/45.0
+#define a42 -56.0/15.0
+#define a43 32.0/9.0
+
+#define a51 19372.0/6561.0
+#define a52 -25360.0/2187.0
+#define a53 64448.0/6561.0
+#define a54 -212.0/729.0
+
+#define a61 9017.0/3168.0
+#define a62 -355.0/33.0
+#define a63 46732.0/5247.0
+#define a64 49.0/176.0
+#define a65 -5103.0/18656.0
+
+#define a71 35.0/384.0
+#define a72 0.0
+#define a73 500.0/1113.0
+#define a74 125.0/192.0
+#define a75 -2187.0/6784.0
+#define a76 11.0/84.0
+
+#define b1 35.0/284.0
+#define b2 0.0
+#define b3 500.0/1113.0
+#define b4 125.0/192.0
+#define b5 -2187.0/6784.0
+#define b6 11.0/84.0
+
+#define bs1 5179.0/57600.0
+#define bs2 0.0
+#define bs3 7571.0/16695.0
+#define bs4 393.0/640.0
+#define bs5 -92097.0/339200.0
+#define bs6 187.0/2100.0
+#define bs7 1.0/40.0
