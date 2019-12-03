e20ed751318ff281760887b5c4c6ad661edf228e
Beatriz Estrada <beatrizestrada01@gmail.com>
2019-12-03 19:51:14 +0000

    updated for a better plot

--- semi_major_plot.py
+++ semi_major_plot.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D 
 
 
 plt.ioff()
@@ -8,27 +7,104 @@ plt.ioff()
 
 #construct dataset to plot
 time = []
-a = []
+semi6 = []
+semi7 = []
+semi8 = []
+semi9 = []
+semi10 = []
+semi11 = []
+semi12 = []
 
-dataset = open('rk_data.txt','r')
 
-for line in dataset:
+ds6 = open('data6.txt','r')
+
+for line in ds6:
     line = line.strip()
-    X, Y = line.split(',')
-    time.append(float(X))
-    a.append(float(Y))
-    
-dataset.close()
+    T, A= line.split(',')
+    time.append(float(T))
+    semi6.append(float(A))
+
+ds6.close()
+
+ds7 = open('data7.txt', 'r')
+
+for line in ds7:
+    line = line.strip()
+    T, A= line.split(',')
+    #time.append(float(T))
+    semi7.append(float(A))
+
+ds7.close()
+
+ds8 = open('data8.txt', 'r')
+
+for line in ds8:
+    line = line.strip()
+    T, A= line.split(',')
+    #time.append(float(T))
+    semi8.append(float(A))
+
+ds8.close()
+
+ds9 = open('data9.txt', 'r')
+
+for line in ds9:
+    line = line.strip()
+    T, A= line.split(',')
+    #time.append(float(T))
+    semi9.append(float(A))
+
+ds9.close()
+
+ds10 = open('data10.txt', 'r')
+
+for line in ds10:
+    line = line.strip()
+    T, A= line.split(',')
+    #time.append(float(T))
+    semi10.append(float(A))
+
+ds10.close()
+
+ds11 = open('data11.txt', 'r')
+
+for line in ds11:
+    line = line.strip()
+    T, A= line.split(',')
+    #time.append(float(T))
+    semi11.append(float(A))
+
+ds11.close()
+
+ds12 = open('data12.txt', 'r')
+
+for line in ds12:
+    line = line.strip()
+    T, A= line.split(',')
+    #time.append(float(T))
+    semi12.append(float(A))
+
+ds12.close()
+
+
 
 true_a = []
 for i in range(len(time)):
     true_a.append(1.0)
-    
+
 
 fig, ax = plt.subplots()
 
-ax.plot(time, a, marker = '.', c = 'blue')
-ax.plot(time, true_a, 'r-')
 
-plt.savefig('semimajor.png')        
+ax.plot(time, semi7, marker = '.', c = '#0061E8', linewidth = 0.0, label = '1e-7')
+ax.plot(time, semi8, marker = '.', c = '#D20000', linewidth = 0.0, label = '1e-8')
+ax.plot(time, semi9, marker = '.', c = '#C838C0', linewidth = 0.0, label = '1e-9')
+ax.plot(time, semi10, marker = '.', c = '#00CFFF', linewidth = 0.0, label = '1e-10')
+ax.plot(time, semi11, marker = '.', c = '#00B900', linewidth = 0.0, label = '1e-11')
+ax.plot(time, semi12, marker = '.', c = '#FF9300', linewidth = 0.0, label = '1e-12')
 
+ax.legend()
+plt.xlabel('Number of orbits')
+plt.ylabel('Semi-major axis relative error')
+
+plt.savefig('semimajor.png')
