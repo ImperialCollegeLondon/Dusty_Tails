a572edb1e7e3d592bcebbe621eed11851ba00db0
Anna Wilson <annacharis@hotmail.co.uk>
2020-03-17 14:47:09 +0000

    new stuff

--- Final/reader.cpp
+++ Final/reader.cpp
@@ -15,33 +15,37 @@ struct DataPoint {
    double psize;
  }
 
-void read_binary(string file_name){
-  file.open(file_name.c_str(),ios::binary);
-  if(!file.is_open())
-  {
-     cerr << "File noT found \n";
-     exit(EXIT_FAILURE);
-  }
-  // ifstream file_to_open(file_name.c_str(), ios::binary);
-  // ofstream file_to_write("output.txt");
-
-  DataPoint data;
-
-  file.read((char*) &data,sizeof(data));
-
-  // for(int i = 0; i < 3; i++)
-  //     file_to_write.write((char *) &time[i], sizeof(DataPoint));
-  //  file_to_write.close();
-  // float value;
-  //
-  //   if(file_to_open.is_open()){
-  //     while(!file_to_open.eof()){
-  //       file_to_open.read((char*)&value,sizeof(float));
-  //       file_to_write << value << "\n";
-  //
-  //     }
-  //   }
+void read_binary(string file){
+  file.open(file.c_str(),ios::binary);
 }
+
+// void read_binary(string file_name){
+//   file.open(file_name.c_str(),ios::binary);
+//   if(!file.is_open())
+//   {
+//      cerr << "File noT found \n";
+//      exit(EXIT_FAILURE);
+//   }
+//   // ifstream file_to_open(file_name.c_str(), ios::binary);
+//   // ofstream file_to_write("output.txt");
+//
+//   DataPoint data;
+//
+//   file.read((char*) &data,sizeof(data));
+//
+//   // for(int i = 0; i < 3; i++)
+//   //     file_to_write.write((char *) &time[i], sizeof(DataPoint));
+//   //  file_to_write.close();
+//   // float value;
+//   //
+//   //   if(file_to_open.is_open()){
+//   //     while(!file_to_open.eof()){
+//   //       file_to_open.read((char*)&value,sizeof(float));
+//   //       file_to_write << value << "\n";
+//   //
+//   //     }
+//   //   }
+// }
 
  int main() {
    read_binary("/Users/annawilson/Downloads/output_optical_depth.bin");
