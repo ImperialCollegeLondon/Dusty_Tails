bbb7ab3e3928a7e850249bf2de6ea03088ce6c9b
Beatriz Estrada <beatrizestrada01@gmail.com>
2020-03-31 16:02:25 +0100

    change in file writing

--- Solver_files/add_rm.cpp
+++ Solver_files/add_rm.cpp
@@ -16,7 +16,7 @@ unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
 mt19937 generator (seed);
 uniform_real_distribution<double> uniform01(0.0, 1.0);
 
-ofstream ofile("output_debug.bin", ios::out | ios::binary);
+ofstream ofile("output_035.bin", ios::out | ios::binary);
 
 vector <Particle> add_particles(vector <Particle> particles, long int current,
                    long int total, double time){
@@ -52,13 +52,14 @@ vector <Particle> add_particles(vector <Particle> particles, long int current,
                  particles.push_back(grain);
 
 
-                 ofile.write((char*) &time, sizeof(double));
-                 ofile.write((char*) &grain.id, sizeof(long int));
-                 ofile.write((char*) &grain.position[0], sizeof(double));
-                 ofile.write((char*) &grain.position[1], sizeof(double));
-                 ofile.write((char*) &grain.position[2], sizeof(double));
-                 ofile.write((char*) &grain.p_size, sizeof(double));
-                 ofile.write((char*) &grain.p_mass, sizeof(double));
+                 //ofile.write((char*) &time, sizeof(double));
+                 //ofile.write((char*) &grain.id, sizeof(long int));
+                 //ofile.write((char*) &grain.position[0], sizeof(double));
+                 //ofile.write((char*) &grain.position[1], sizeof(double));
+                 //ofile.write((char*) &grain.position[2], sizeof(double));
+                 //ofile.write((char*) &grain.p_size, sizeof(double));
+                 //ofile.write((char*) &grain.p_mass, sizeof(double));
+
 
                }
 
@@ -100,26 +101,36 @@ void solve_particles(double total_t, double end_t, vector <Particle> particles, 
   while (total_t < end_t) {
 
     for( Particle& p : particles) {
-      vector <double> updated_vector;
-      updated_vector.clear();
-      updated_vector = RK_solver({p.position[0], p.position[1], p.position[2], \
-      p.velocity[0], p.velocity[1], p.velocity[2], p.p_size}, total_t, t_common, p.h_updated);
 
-      p.position = {updated_vector[0],updated_vector[1], updated_vector[2]};
-      p.velocity = {updated_vector[3],updated_vector[4], updated_vector[5]};
-      p.p_size = updated_vector[6];
-      p.p_mass = dust_mass(p.p_size);
-      p.h_updated = updated_vector[7];
-      
-      double time_now = total_t + big_step;
+        ofile.write((char*) &total_t, sizeof(double));
+        ofile.write((char*) &p.id, sizeof(long int));
+        ofile.write((char*) &p.position[0], sizeof(double));
+        ofile.write((char*) &p.position[1], sizeof(double));
+        ofile.write((char*) &p.position[2], sizeof(double));
+        ofile.write((char*) &p.p_size, sizeof(double));
+        ofile.write((char*) &p.p_mass, sizeof(double));
 
-      ofile.write((char*) &time_now, sizeof(double));
-      ofile.write((char*) &p.id, sizeof(long int));
-      ofile.write((char*) &p.position[0], sizeof(double));
-      ofile.write((char*) &p.position[1], sizeof(double));
-      ofile.write((char*) &p.position[2], sizeof(double));
-      ofile.write((char*) &p.p_size, sizeof(double));
-      ofile.write((char*) &p.p_mass, sizeof(double));
+        if (isnan(p.position[0])) {
+            cout << "id " << p.id << endl;
+
+        }
+
+        vector <double> updated_vector;
+        updated_vector.clear();
+        updated_vector = RK_solver({p.position[0], p.position[1], p.position[2], \
+        p.velocity[0], p.velocity[1], p.velocity[2], p.p_size}, total_t, t_common, p.h_updated);
+        p.position = {updated_vector[0],updated_vector[1], updated_vector[2]};
+        p.velocity = {updated_vector[3],updated_vector[4], updated_vector[5]};
+        p.p_size = updated_vector[6];
+        p.p_mass = dust_mass(p.p_size);
+        p.h_updated = updated_vector[7];
+
+
+
+
+        //double time_now = total_t + big_step;
+
+
 
     }
 
