eccfe0d0aea28bea3a15341bec36e9e56f42b621
Beatriz Estrada <beatrizestrada01@gmail.com>
2020-07-02 01:16:29 +0100

    deleted some files

--- Solver_files/read_bin.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-dt = np.dtype([('time', np.float64), ('id', np.int64), ('x', np.float64), \
-('y', np.float64), ('z', np.float64)])
-
-data = np.fromfile("test.bin", dt)
-
-print(data)
