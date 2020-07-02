8dcc2a81d546c18f839a81311e76ff49c073c56b
Beatriz Estrada <beatrizestrada01@gmail.com>
2020-07-02 01:09:33 +0100

    new changes to error calculation

--- Solver_files/errors.cpp
+++ Solver_files/errors.cpp
@@ -42,15 +42,15 @@ double h_optimal(vector <double> deltas_list, double h){
 }
 
 vector <double> h_check(double h, vector <double> V){
-    vector <double> deltas(6);
-    vector <double> order4(6), order5(6);
+    vector <double> deltas(7);
+    vector <double> order4(7), order5(7);
 
     //4th order
     order4 = new_variables(h, V, false);
     //5th order
     order5 = new_variables(h, V, true);
 
-    for (unsigned int i = 0; i < 6; i++){
+    for (unsigned int i = 0; i < 7; i++){
         deltas[i] = delta(order4[i], order5[i]);
     }
     return deltas;
