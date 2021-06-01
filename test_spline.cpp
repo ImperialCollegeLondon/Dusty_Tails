538677fcd49132549806f7a00c8ad6bcf0a30b15
beatrizestrada <beatrizestrada01@gmail.com>
2021-06-01 16:33:06 +0200

    added possible change to make interpolation faster

--- test_spline.cpp
+++ test_spline.cpp
@@ -41,6 +41,57 @@ double tricubicInterpolate (vector <vector< vector <double> > > p, vector <doubl
     return cubicInterpolate(arr, s1, x);
 }
 
+vector < vector < tk:: spline > > splines_phi (vector <vector< vector <double> > > taus, vector <double> radii, vector <double> thetas, vector <double> phis) {
+    vector < vector < tk::spline >> phi_splines;
+    tk::spline s; 
+    for (int i = 0; i<radii.size(); i++){
+        phi_splines.push_back({});
+        for ( int j=0; j<thetas.size(); j++) {
+            tk::spline s(phis, taus[i][j], tk::spline::cspline);
+            phi_splines[i].push_back(s);
+        }
+    }
+
+    return phi_splines;
+}
+
+vector <vector <double> > phi_spline_result(vector < vector < tk:: spline >> splines , vector<double> radii, vector <double> thetas, vector<double> phis, double phi) {
+    vector <vector <double> > phi_spline_values;
+    for (int i = 0; i<radii.size(); i++){
+        phi_spline_values.push_back({});
+        for ( int j=0; j<thetas.size(); j++) {
+            phi_spline_values[i].push_back(splines[i][j](phi));
+        }
+    } 
+    return phi_spline_values;
+}
+vector < tk:: spline > splines_theta ( vector <vector <double>> phi_splines_p, vector<double> radii, vector<double> thetas) {
+     
+    vector < tk::spline > r_splines;
+    for (int i =0; i < radii.size(); i++){
+        tk::spline s(thetas, phi_splines_p[i], tk::spline::cspline);
+        r_splines.push_back(s);
+    }
+
+    return r_splines;
+}
+
+vector <double> theta_spline_result( vector <tk:: spline> splines, vector<double> radii, vector<double> thetas, vector<double> phis , double theta) {
+    vector <double>  theta_spline_values;
+    for (int i = 0; i<radii.size(); i++){
+            theta_spline_values.push_back(splines[i](theta));
+       
+        }
+    return theta_spline_values;
+    } 
+    
+
+double tau_p (vector<double> theta_splines_p, vector<double> radii, vector<double> thetas, vector<double> phis, double radius){
+    tk:: spline s(radii, theta_splines_p);
+    
+    return s(radius);
+}
+
 
 int main() {
     
@@ -65,16 +116,45 @@ int main() {
         double p = distribution_p(generator);
         points.push_back({r,t,p});
     }
-    auto start = std::chrono::high_resolution_clock::now();
+    //auto start = std::chrono::high_resolution_clock::now();
     
     for (int i = 0; i< 15000; i++) {
         vector <double> point = points[i];
         tricubicInterpolate(tau, xs, ys, zs, point[0], point[1], point[2]);
     }
     
+    //auto finish = std::chrono::high_resolution_clock::now();
+
+    //td::chrono::duration<double> elapsed = finish - start;
+
+    //std::cout << "Elapsed time: " << elapsed.count() << " s\n";
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    vector< vector <tk::spline >> s_phi;
+
+    s_phi = splines_phi (tau, xs, ys, zs);
+
+    vector < vector <double> > s_phi_values;
+    s_phi_values = phi_spline_result(s_phi , xs, ys, zs, 1.0);
+
+    vector < tk:: spline > s_theta;
+
+    s_theta =  splines_theta ( s_phi_values, xs, ys);
+
+    vector <double> s_theta_values;
+
+    s_theta_values = theta_spline_result( s_theta, xs, ys, zs , 2.0);
+
+    double result;
+    result =  tau_p (s_theta_values, xs, ys, zs, 4.0);
     auto finish = std::chrono::high_resolution_clock::now();
 
-    std::chrono::duration<double> elapsed = finish - start;
+    cout << result << endl;
 
+    std::chrono::duration<double> elapsed = finish - start;
     std::cout << "Elapsed time: " << elapsed.count() << " s\n";
 }
+
+
+
