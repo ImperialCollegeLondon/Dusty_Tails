2f128ed04a73bdb93d224624c1015978f44dbd5c
beatrizestrada <beatrizestrada01@gmail.com>
2022-09-06 17:54:02 +0200

    added folder with source code

---
+++ src/constants.h
@@ -0,0 +1,86 @@
+//Header file with constants
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include "spline.h"
+
+
+using namespace std;
+
+#define PI 3.14159
+#define wien 0.289 //Wiens constant in cgs
+#define G 6.67408e-11 //gravitational constant
+#define Msun 1.9885e+30 //solar mass in kg
+#define Rsun 6.9551e+8 //solar radius in meters
+#define AU_to_m 1.496e+11 //AU in meters
+#define m_to_AU 6.68459e-12 //meters in AU
+#define Mearth 5.972e+24 //Earth mass in kg
+#define clight 2.9979245e+8 //speed of light in SI
+#define Rearth 6.3781e+6 //Earth radius in meters
+
+//definition of the grid cell numbers for the optical depth grid
+const int r_cells = 120;
+const double r_cells_d = 120.;
+const int t_cells = 50;
+const double t_cells_d = 50.;
+const int p_cells =350;
+const double p_cells_d = 350.;
+
+extern double t_init;
+//declaration of some external variables
+extern double star_x; //x position of the star, w.r.t CoM of system
+extern double planet_x; //x position of the planet, w.r.t CoM of system
+extern vector <double> star_pos; //vector of the stars position
+extern vector <double> planet_pos; //vector of the planets position
+
+//Frame of reference parameters
+extern double ang_vel;
+
+//Stellar parameters:
+extern  double Mstar_cgs, Mstar_sun, Rstar, Temp, lum; 
+extern double r_star_a;
+//Planetary parameters:
+extern  double Period_days, T, a, m_planet, r_planet, r_start, r_planet_dim, r_h, b_p;
+
+//Dust parameters:
+extern  double A, Bp, rho_d, s_0,alpha, mu;
+extern  string opac_data, opacity_dir;
+extern  string comp, outflow_s, T_int_s, output_file;
+
+//Outflow parameters:
+extern double mdot, v_esc, mdot_read, v_th, mu_gas;
+extern int outflow, tau_type;
+extern bool tau_constant;
+extern int cont;
+extern double nparticles, major_timestep;
+extern double n_mini, m_big;
+extern double t0;
+//Grid parameters:
+extern double d_r_min, d_r_max,  d_t_min, d_t_max, d_p_min, d_p_max;
+
+extern long int current_particles, total_particles; // number of current particles in simulation
+extern double dr, dtheta, dphi;
+extern double d_dr, d_dtheta, d_dphi;
+
+//Some dimensionless quantitites:
+extern double G_dim, c_dim;
+
+//Numerical factors for integrator:
+const double S = 0.9; //safety factor
+const double tol = 1.0e-7; //error tolerance
+
+//GYR in seconds
+const double gyr = pow(10.,9) * 365. * 24. * 60. * 60. ;// 1Gyr in seconds
+
+//constants in cgs units for beta calculation
+#define amu 1.661e-24
+#define kb 1.381e-16
+#define sigma 5.6705119e-5 //stefan boltzmann
+#define Rsun_cgs 6.96e+10 //solar radius
+#define G_cgs 6.67259e-8 //gravitational constant
+#define c_cgs 2.99792458e+10 //speed of light
+#define Msun_cgs 1.9885e+33 // solar mass
+#define Mearth_cgs 5.972e+27 // earth mass grams
+#define Rearth_cgs 6.378e+8 // earth radius cm
+#define clight_cgs 2.9979245e+10 //speed of light in cgs
+#define pc 3.086e+18 //parsec in cm
---
+++ src/particle.h
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include "spline.h"
+
+using namespace std;
+
+class Particle {
+   public:
+    long int id; //unique id of the particle
+    vector <double> position; //current position of the particle
+    vector <double> velocity; //current velocity of the particle
+    vector <double> pos_spherical; // current position in spherical coordinates
+    vector <double> v_spherical; //curretn velocity in spherical coordinates
+    vector <double> pos_p; //firt projection for lc calculation
+    vector <double> pos_dp; //second projection for lc calculation
+    double size; //size of the particle
+    double opac_abs;
+    double opac_scat; 
+    double opac_planck;
+    double gsca;
+    double h_updated; //current optimal time step for particle
+    double mass; //mass of particle
+    double tau_d; //optical depth
+    double temp_d; //particle temperature
+    double f_scat;
+
+};
+
+void add_particles(vector <Particle> &particles,long int &current_particles, long int &total_particles, double total_t);
+
+
+
+void solve_particles(double total_t, double end_t, vector <Particle> &particles, \
+                     long int total_particles,long int current_particles);
+
+void rm_particles(vector <Particle> &particles);
+
+void calculation_ext(vector <Particle>& particles, double (&ext)[r_cells][t_cells][p_cells],  double delta_t);
+
+//light curve
+
+void light_curve(vector<Particle>& particles, double current_t);
+void  extinction_lc( vector <Particle>& particles, vector <vector <double>> &patches, 
+         vector<double> &h_grid, vector<double> &v_grid,
+         vector<vector <double>> &taus, int h_cells, int v_cells,
+         double z_max, double z_min, double y_max, double y_min, double r_star_a);
+
+
+extern vector <Particle> particles;
+extern bool tau_constant;
